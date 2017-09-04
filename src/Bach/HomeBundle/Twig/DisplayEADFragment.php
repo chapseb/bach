052c32b26d5353c080884a31c868da55c1fb5e41
Sebastien Chaptal <sebastien.chaptal@anaphore.eu>
2017-09-04 11:57:49 +0200

    Fix highlight empty in scope content case

--- src/Bach/HomeBundle/Twig/DisplayEADFragment.php
+++ src/Bach/HomeBundle/Twig/DisplayEADFragment.php
@@ -357,44 +357,45 @@ class DisplayEADFragment extends \Twig_Extension
                     $matches
                 );
 
-                $getLinkScopecontent = preg_match_all(
-                    '/<a href=\"(.*?)\>(.*?)<\/a>/',
-                    $matches[0][0],
-                    $matchesScopecontent
-                );
-
-                if (!empty($matches[0][0])) {
-                    $result = $matches[0][0];
-                    foreach ($allWord as $wordHigh) {
-                        if (!strpos('<em class="hl">', $wordHigh)) {
-                            $result = str_replace(
-                                $wordHigh,
-                                '<em class="hl">'.$wordHigh.'</em>',
-                                $result
-                            );
-                        }
-                    }
-                    $text = str_replace($matches[0][0], $result, $text);
-                }
-                $result = preg_match_all(
-                    '@<section class="scopecontent"[^>]*?>.*?</section>@si',
-                    $text,
-                    $matches
-                );
-
-                $getBadLinkScopecontent = preg_match_all(
-                    '/<a href=\"(.*?)\>(.*?)<\/a>/',
-                    $matches[0][0],
-                    $matchesBadScopecontent
-                );
-                foreach ($matchesBadScopecontent[0] as $key => $badScopecontent) {
-                    $text = str_replace(
-                        $badScopecontent,
-                        $matchesScopecontent[0][$key],
-                        $text
+                if (!empty($matches[0])) {
+                    $getLinkScopecontent = preg_match_all(
+                        '/<a href=\"(.*?)\>(.*?)<\/a>/',
+                        $matches[0][0],
+                        $matchesScopecontent
                     );
-                }
 
+                    if (!empty($matches[0][0])) {
+                        $result = $matches[0][0];
+                        foreach ($allWord as $wordHigh) {
+                            if (!strpos('<em class="hl">', $wordHigh)) {
+                                $result = str_replace(
+                                    $wordHigh,
+                                    '<em class="hl">'.$wordHigh.'</em>',
+                                    $result
+                                );
+                            }
+                        }
+                        $text = str_replace($matches[0][0], $result, $text);
+                    }
+                    $result = preg_match_all(
+                        '@<section class="scopecontent"[^>]*?>.*?</section>@si',
+                        $text,
+                        $matches
+                    );
+
+                    $getBadLinkScopecontent = preg_match_all(
+                        '/<a href=\"(.*?)\>(.*?)<\/a>/',
+                        $matches[0][0],
+                        $matchesBadScopecontent
+                    );
+                    foreach ($matchesBadScopecontent[0] as $key => $badScopecontent) {
+                        $text = str_replace(
+                            $badScopecontent,
+                            $matchesScopecontent[0][$key],
+                            $text
+                        );
+                    }
+                }
             }
         }
 
